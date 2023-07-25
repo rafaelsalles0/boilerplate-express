b985a6f1085a8291d6353468949144df9431574e
rafaelsalles0 <76059989+rafaelsalles0@users.noreply.github.com>
2023-07-24 21:37:29 -0300

    last version

--- myApp.js
+++ myApp.js
@@ -1,16 +1,75 @@
 let express = require('express');
 let app = express();
-
-console.log("Hello World")
-
-app.get("/", function (req, res) {
-  res.send('Hello Express');
-});
-
-
+var bodyParser = require('body-parser');
+require('dotenv').config()
+//console.log("Hello World")
+//let path = __dirname + '/public'
+//app.use(express.static('public'));
+// Normal usage
 
 
 
+app.use(express.static(__dirname + "/public"));
+
+// Assets at the /public route
+app.use("/public", express.static(__dirname + "/public"));
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.post("/name", function(req, res) {
+    // Handle the data in the request
+    var string = req.body.first + " " + req.body.last;
+    res.json({ name: string });
+  });
+//app.use(console.log(bodyParser.json()));
+
+
+app.route("/name").get(function(req,res,next){
+    var firstName = req.query.first;
+    var lastName = req.query.last;
+    // OR you can destructure and rename the keys
+    var { first: firstName, last: lastName } = req.query;
+    // Use template literals to form a formatted string
+    res.json({
+      name: `${firstName} ${lastName}`
+    });
+    next();
+}).post(function(req, res){
+    res.json({name: firstname +" "+ lastname})
+    console.log("Entrou!")
+})
+
+app.get('/:word/echo', function(req,res,next){
+
+    let word = req.params.word;
+    //let echo = req.params.echo;
+    res.json({echo: word})
+})
+
+app.get('/now', function(req, res, next) {
+    req.time = new Date().toString();
+    next();
+}, function(req, res){
+    res.json({"time": req.time})
+})
+
+app.use(function middleware(req, res, next) {
+    console.log(req.method + " " + req.path + " - " + req.ip);
+    next();
+})
+
+
+
+app.get("/json", (req, res) => {
+    //console.log(process.env.MESSAGE_STYLE)
+    if(process.env.MESSAGE_STYLE == "uppercase"){
+        res.json({"message": "HELLO JSON"})
+    }else{
+        res.json({"message": "Hello json"})
+    }
+    
+  });
 
 
 
@@ -38,4 +97,14 @@ app.get("/", function (req, res) {
 
 
 
- module.exports = app;
+
+
+
+
+
+
+
+
+
+
+module.exports = app;
